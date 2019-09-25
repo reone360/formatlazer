4e15870a9cd7b8e6c02051df1d6267e727136b44
reone360 <spyros360reo@gmail.com>
2019-09-25 21:41:11 +0300

    -added css to main page
    -added css styling to main page
    -modified page to show the textareas
    -added submit button
    -added slight detail to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,8 @@
 <body>
     <strong>ABOUT PAGE - UNDER DEVELOPMENT</strong>
+    <br>
+    <br>
+    <strong>Developed by Spyros</strong>
+    <br>
+    <strong>for any inquiries, contact spyros360reo@gmail.com</strong>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,8 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #424242;
+                color: #d2bbf3;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
@@ -52,7 +52,7 @@
                 color: #636b6f;
                 padding: 0 25px;
                 font-size: 13px;
-                font-weight: 600;
+                font-weight: 400;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
@@ -61,40 +61,66 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            textarea {
+                position: absolute;
+                left: 38%;
+                margin: auto;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                border-radius: 20px 20px 0 0;
+                background-color: #717171;
+                border-color: #272727;
+            }
+
+            .inputArea {
+                top: 30%;
+            }
+
+            .outputArea{
+                top: 60%;
+            }
+
+            .transformbtn{
+                position: absolute;
+                top: 43%;
+                left: 38%;
+                width: 542px;
+                height: 73px;
+                text-align: center;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 600;
+                border-radius: 0px 0px 20px 20px;
+                font-size: 18px;
+                background-color: #171717;
+                border-color: #171717;
+                color: #d2bbf3;
+            }
+            h1{
+                position: absolute;
+                left: 39%;
+                top: 23%;
+            }
+
+            h2{
+                position: absolute;
+                left: 46%;
+                top: 55%;
+            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <h1>Enter your text below to Formalize</h1>
+        <form>
+            <textarea name="inputArea" class="inputArea" style="resize: none;" rows="10" cols="50" autofocus ></textarea>
+            <input class="transformbtn" type="submit" value="Transform">
+        </form>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+        <h2>Formal result</h2>
+        <textarea name="outputArea" class="outputArea" style="resize: none;" rows="10" cols="50" autofocus ></textarea>
     </body>
 </html>
